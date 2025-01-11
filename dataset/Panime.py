4b729ed9a482f0722229b5f97092ac045d864e4d
mohaned5 <mohaned@live.co.uk>
2025-01-11 19:16:47 +0000

    Fix loader for larger batch size

--- dataset/Panime.py
+++ dataset/Panime.py
@@ -115,9 +115,9 @@ class PanimeDataset(PanoDataset):
 
             # 3) Build 'cameras' dict in the format PanFusion expects
             cam_data = data['cameras_data']
-            FoV = np.array(cam_data['FoV'], dtype=np.float32)
-            theta = np.array(cam_data['theta'], dtype=np.float32)
-            phi = np.array(cam_data['phi'], dtype=np.float32)
+            FoV = np.array(cam_data['FoV'], dtype=np.float32).squeeze()
+            theta = np.array(cam_data['theta'], dtype=np.float32).squeeze()
+            phi = np.array(cam_data['phi'], dtype=np.float32).squeeze()
 
             cameras = {
                 # If you want each perspective to be 256×256, set these to pers_resolution
